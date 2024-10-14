9a95ded14fd4c6ee6983c743dea5e8b3ca5eb834
Pranathi-pamu <chinnupamu11@gmail.com>
2024-10-14 15:23:29 +0530

    Add files via upload

---
+++ prime or not.c
@@ -0,0 +1,50 @@
+#include <math.h>
+
+int main() 
+{
+     int  i,n,count=0;
+     printf("enter the  n value ");
+     scanf("%d",&n);
+     for(i=1;i<=n;i++)
+     { 
+     (n%i==0);
+     count=count+1;
+     
+     
+     }
+     if(count==2)
+     
+     printf(" %d is prime no");
+     else 
+     printf(" %d is not a prime no");
+     
+     
+     
+     
+     
+     
+    
+  
+ 
+       
+         
+       
+         
+       
+       
+     
+     
+      
+      
+      
+      
+      
+     
+     
+      
+ }    
+ 
+     
+
+
+    
